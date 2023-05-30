043da081d818caa37c4973b27168dce2d6fab0bf
ThePurpleRaven <otpxayaheuw@gmail.com>
2023-05-30 13:34:04 +0200

    Mulisium Folder

---
+++ Mulisium/PickUpObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObject : Interactable
+{
+    public override void OnInteract()
+    {
+        //Do something
+    }
+}
